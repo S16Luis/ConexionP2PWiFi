5e59276e9261c741386e61202bbe675d1bffc80e
S16Luis <37001738+S16Luis@users.noreply.github.com>
2021-04-23 22:54:44 -0500

    Roles Cliente/Servidor implementados

--- app/src/main/java/com/example/conexionp2p/MainActivity.java
+++ app/src/main/java/com/example/conexionp2p/MainActivity.java
@@ -26,7 +26,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -149,6 +153,43 @@ public class MainActivity extends AppCompatActivity {
     public void onPause() {
         super.onPause();
         unregisterReceiver(mReceiver);
+    }
+
+    public class ServerClass extends Thread{
+        Socket socket;
+        ServerSocket serverSocket;
+
+        @Override
+        public void run(){
+            try {
+                serverSocket = new ServerSocket(88888);
+                socket = serverSocket.accept();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class ClientClass extends Thread{
+        Socket socket;
+        String hostAdd;
+
+        public ClientClass(InetAddress hostAddress)
+        {
+            hostAdd = hostAddress.getHostAddress();
+            socket = new Socket();
+        }
+
+        @Override
+        public void run(){
+            try {
+                socket.connect(new InetSocketAddress(hostAdd,8888),500);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void ControlWifi(View v)
